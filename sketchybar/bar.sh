7c7199bf571ea0d5c8af952fb095cd09754e99ad
Xibo Wang <xibo.wang@vudoo.com>
2025-01-13 09:23:11 +1100

    update sketchybar

--- sketchybar/bar.sh
+++ sketchybar/bar.sh
@@ -10,7 +10,7 @@ source "$CONFIG_DIR/variables.sh"
 # https://felixkratz.github.io/SketchyBar/config/tricks#color-picker
 sketchybar --bar position=top \
                  height=$BAR_HEIGHT \
-                 blur_radius=8 \
-                 color=0x40000000 \
+                 blur_radius=$BLUR_RADIUS \
+                 color=$BAR_BACKGROUND_COLOR \
                  padding_left=$BAR_PADDING_INLINE \
                  padding_right=$BAR_PADDING_INLINE
--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -5,7 +5,7 @@ source $CONFIG_DIR/variables.sh
 sketchybar --set $NAME label="$($UTILS_DIR/get-space-label.sh $1)"
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set $NAME background.color="0xffac4d39"
+  sketchybar --set $NAME background.color="$FOCUSED_WORKSPACE_COLOR"
 else
-  sketchybar --set $NAME background.color=0xff2f3e46
+  sketchybar --set $NAME background.color="$UNFOCUSED_WORKSPACE_COLOR"
 fi
