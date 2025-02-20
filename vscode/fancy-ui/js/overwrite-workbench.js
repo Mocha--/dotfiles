5cb4b4d301b86fd76a9fa0f490eabd914b025bce
Xibo Wang <xibo.wang@vudoo.com>
2025-02-20 15:52:14 +1100

    vscode: update overwrite-workbench

--- vscode/fancy-ui/js/overwrite-workbench.js
+++ vscode/fancy-ui/js/overwrite-workbench.js
@@ -1,51 +1,49 @@
-const basePath = '../../../../'
+const quickInputWidgetLineItemHeight = 40;
+const sidebarLineItemHeight = 40;
 
-const loadJsFile = async (path, callback, isImport) => {
-	const resp = await fetch(
-		(path.startsWith('./') ? path : basePath + path) + '.js'
-	)
-	const respText = await resp.text()
-	const newText = callback ? await callback(respText) : respText
-	const blob = new Blob([newText], { type: 'application/javascript' })
-	const url = URL.createObjectURL(blob)
-
-	return isImport ? await import(url) : url
-}
-
-const replaceCommandPannelHeight = (text) => {
-	const commandPannelHeight = 40;
-	// command panel item height
-	// getHeight(i){return i instanceof iO?30:i.saneDetail?44:22}
-	text = text.replace(
-		/(44:)22(?=})/g,
-		(_, prefix) => `${prefix}${commandPannelHeight}`
-	)
-	return text
-}
-
-const replaceItemHeight = (text) => {
-	const itemHeight = 40
-	// item height
-	let replaceItemH = (_, prefix) => `${prefix}${itemHeight}`
-	text = text.replace(/(eight\([^)]*?\)\{return )22(?!\d)/g, replaceItemH)
-	text = text.replace(/(ITEM_HEIGHT=)22(?!\d)/g, replaceItemH)
-	text = text.replace(/(\*)22(?!\d)/g, replaceItemH)
-	text = text.replace(/(:)22(?=})/g, replaceItemH)
-	return text
-}
-
-;(async function () {
-  const mainJsPath = 'vs/workbench/workbench.desktop.main'
-  await loadJsFile(
-    './workbench',
-    async (text) => {
-      const mainJsUrl = await loadJsFile(mainJsPath, (text) => {
-        text = replaceCommandPannelHeight(text)
-        text = replaceItemHeight(text)
-        return text
-      })
-      return text.replace(mainJsPath, mainJsUrl + '#')
-    },
-    true
+async function loadJsFile(path, callback, isImport) {
+  const basePath = '../../../../';
+  const resp = await fetch(
+    (path.startsWith('./') ? path : basePath + path) + '.js'
   )
-})()
+  const respText = await resp.text();
+  const newText = callback ? await callback(respText) : respText;
+  const blob = new Blob([newText], { type: 'application/javascript' });
+  const url = URL.createObjectURL(blob);
+
+  return isImport ? await import(url) : url;
+}
+
+function replaceQuickInputWidgetLineItemHeight(text, lineItemHeight) {
+  // command panel item height
+  // getHeight(i){return i instanceof iO?30:i.saneDetail?44:22}
+  return text.replace(
+    /(44:)22(?=})/g,
+    (_, prefix) => `${prefix}${lineItemHeight}`
+  );
+}
+
+function replaceSidebarLineItemHeight(text, lineItemHeight) {
+  // item height
+  const replacement = (_, prefix) => `${prefix}${lineItemHeight}`
+  return text
+    .replace(/(eight\([^)]*?\)\{return )22(?!\d)/g, replacement)
+    .replace(/(ITEM_HEIGHT=)22(?!\d)/g, replacement)
+    .replace(/(\*)22(?!\d)/g, replacement)
+    .replace(/(:)22(?=})/g, replacement);
+}
+
+await loadJsFile(
+  './workbench',
+  async (text) => {
+    const mainJsPath = 'vs/workbench/workbench.desktop.main'
+    const mainJsUrl = await loadJsFile(mainJsPath, text =>
+      replaceSidebarLineItemHeight(
+        replaceQuickInputWidgetLineItemHeight(text, quickInputWidgetLineItemHeight),
+        sidebarLineItemHeight
+      )
+    );
+    return text.replace(mainJsPath, mainJsUrl + '#');
+  },
+  true
+)
