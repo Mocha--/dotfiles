309359c00ca511d787780ef94967273b8d573c3e
Xibo Wang <xibowangmelb@gmail.com>
2026-04-26 00:38:30 +1000

    claude: remove hook scripts

--- home/.claude/notification-hook.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-curl \
-  -H "Content-Type: application/json" \
-  -d '{"content": "🚨 Waiting for input"}' \
-  https://discordapp.com/api/webhooks/1496498917095768172/z0UTo2Lc1BbxHlTWU4pCSa9sUl3ApPVrTMeCFsjxRzmyQbEeBTOT0mVvOd7qoKQs8Zpn
